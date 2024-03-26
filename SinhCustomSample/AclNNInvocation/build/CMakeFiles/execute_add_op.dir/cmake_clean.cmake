7b5b7d8a708a40dabd3828c170ce989a6e3422cb
jeffding <jeffding890430@163.com>
2024-03-26 03:14:15 +0000

    new ver

---
+++ SinhCustomSample/AclNNInvocation/build/CMakeFiles/execute_add_op.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "/tmp/code/SinhCustomSample/AclNNInvocation/output/execute_add_op"
+  "/tmp/code/SinhCustomSample/AclNNInvocation/output/execute_add_op.pdb"
+  "CMakeFiles/execute_add_op.dir/common.cpp.o"
+  "CMakeFiles/execute_add_op.dir/common.cpp.o.d"
+  "CMakeFiles/execute_add_op.dir/main.cpp.o"
+  "CMakeFiles/execute_add_op.dir/main.cpp.o.d"
+  "CMakeFiles/execute_add_op.dir/op_runner.cpp.o"
+  "CMakeFiles/execute_add_op.dir/op_runner.cpp.o.d"
+  "CMakeFiles/execute_add_op.dir/operator_desc.cpp.o"
+  "CMakeFiles/execute_add_op.dir/operator_desc.cpp.o.d"
+  "CMakeFiles/execute_add_op.dir/tmp/code/SinhCustomSample/SinhCustom/build_out/autogen/aclnn_sinh_custom.cpp.o"
+  "CMakeFiles/execute_add_op.dir/tmp/code/SinhCustomSample/SinhCustom/build_out/autogen/aclnn_sinh_custom.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/execute_add_op.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
