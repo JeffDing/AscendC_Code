d637d1ad774d73176c02103d3bbdba87d9c4423e
jeffding <jeffding890430@163.com>
2024-03-26 12:28:34 +0000

    fix

---
+++ SinhCustomSample/SinhCustom/build_out/op_kernel/CMakeFiles/ops_info_gen_ascend910.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ops_info_gen_ascend910"
+  "tbe/op_info_cfg/ai_core/ascend910/aic-ascend910-ops-info.json"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/ops_info_gen_ascend910.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ SinhCustomSample/SinhCustom/build_out/op_kernel/binary/ascend910/gen/SinhCustom-sinh_custom-0.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "[Ascend910A] Generating SinhCustom_01ec8ddcf6437b12e69898b368caea08 ..."
+opc $1 --main_func=sinh_custom --input_param=/tmp/code/SinhCustomSample/SinhCustom/build_out/op_kernel/binary/ascend910/gen/SinhCustom_01ec8ddcf6437b12e69898b368caea08_param.json --soc_version=Ascend910A --output=$2 --impl_mode="" --simplified_key_mode=0 --op_mode=dynamic
+
+if ! test -f $2/SinhCustom_01ec8ddcf6437b12e69898b368caea08.json ; then
+  echo "$2/SinhCustom_01ec8ddcf6437b12e69898b368caea08.json not generated!"
+  exit 1
+fi
+
+if ! test -f $2/SinhCustom_01ec8ddcf6437b12e69898b368caea08.o ; then
+  echo "$2/SinhCustom_01ec8ddcf6437b12e69898b368caea08.o not generated!"
+  exit 1
+fi
+echo "[Ascend910A] Generating SinhCustom_01ec8ddcf6437b12e69898b368caea08 Done"
