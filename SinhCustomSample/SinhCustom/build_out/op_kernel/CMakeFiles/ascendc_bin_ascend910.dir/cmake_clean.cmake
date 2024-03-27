390b3cc5076cf0ee88680de279244d1c82104d25
jeffding <jeffding890430@163.com>
2024-03-27 17:21:11 +0800

    del build_out

--- SinhCustomSample/SinhCustom/build_out/op_kernel/CMakeFiles/ascendc_bin_ascend910.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/ascendc_bin_ascend910"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ascendc_bin_ascend910.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- SinhCustomSample/SinhCustom/build_out/op_kernel/CMakeFiles/ascendc_bin_ascend910_sinh_custom_0.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for ascendc_bin_ascend910_sinh_custom_0.
-# This may be replaced when dependencies are built.
